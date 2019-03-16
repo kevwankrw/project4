b1e0a33f54d351ff19f40ca25986eda673316b31
heschmid <47265951+heschmid@users.noreply.github.com>
2019-03-16 16:41:20 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -18,36 +18,49 @@
 
 
 Player::Player() {
-    // TODO: write implementation here.
+	name = "";
+	num_ships = 0;
+	remaining_ships = 0;
+	init_grid();
 }
 
 Player::Player(string name_val) {
-    // TODO: write implementation here.
+	name = name_val;
+	num_ships = 0;
+	remaining_ships = 0;
+	init_grid();
+    
 }
 
 void Player::init_grid() {
-    // TODO: write implementation here.
+	for (int i = 0; i < MAX_GRID_SIZE; i++) {
+		for (int j = 0; j < MAX_GRID_SIZE; j++) {
+			grid[i][j] = EMPTY_LETTER;
+			opponent_grid[i][j] = EMPTY_LETTER;
+		}
+	}
     return;
 }
 
 string Player::get_name() {
-    // TODO: write implementation here.
-    return "";
+    return name;
 }
 
 int Player::get_num_ships() {
-    // TODO: write implementation here.
-    return -1;
+    return num_ships;
 }
 
 int Player::get_remaining_ships() {
-    // TODO: write implementation here.
-    return -1;
+    return remaining_ships;
 }
 
 char Player::get_grid_at(int row, int col) {
-    // TODO: write implementation here.
-    return '?';
+	if (row <= MAX_GRID_SIZE && col <= MAX_GRID_SIZE) {
+		if (row >= 0 && col >= 0) {
+			return grid[row][col];
+		}
+	}
+	return 'F';
 }
 
 char Player::get_opponent_grid_at(int row, int col) {
