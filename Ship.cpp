fed932ed2fe7279a59d0339246c7cbd136f4f118
kevwankrw <47093104+kevwankrw@users.noreply.github.com>
2019-03-17 11:35:23 -0400

    Update Ship.cpp

--- Ship.cpp
+++ Ship.cpp
@@ -4,8 +4,8 @@
  * Ship.cpp
  * Project UID 4cd2d66df3154b6c8b0ff27aa6791edb
  *
- * <#Name#>
- * <#Uniqname#>
+ * <Kevin Wang and Heidi Schmidt>
+ * <kevwan and heschmid>
  *
  * Project 4: Battleship
  *
@@ -18,44 +18,97 @@
 
 
 Ship::Ship() {
-    // TODO: write implementation here.
+	start.set_col = 0;
+	start.set_row = 0;
+	end.set_col = 0;
+	end.set_row = 0;
+	num_hits = 0;
+	size = 0;
 }
 
 Ship::Ship(Position start_in, Position end_in) {
-    // TODO: write implementation here.
+	start = start_in;
+	end = end_in;
+	num_hits = 0;
+	if (start.get_row() == end.get_row()) {
+		size = abs(start.get_col() - end.get_col());
+	}
+	else if (start.get_col() == end.get_col()) {
+		size = abs(start.get_row() - end.get_row());
+	}
 }
 
 Position Ship::get_start() {
-    // TODO: write implementation here.
-    return Position();
+    return start;
 }
 
 Position Ship::get_end() {
-    // TODO: write implementation here.
-    return Position();
+	return end;
 }
 
 int Ship::get_size() {
-    // TODO: write implementation here.
-    return -1;
+	return size;
 }
 
 bool Ship::is_horizontal() {
-    // TODO: write implementation here.
-    return false;
+	if (start.get_row() == end.get_row()) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 bool Ship::has_position(Position pos) {
-    // TODO: write implementation here.
-    return false;
+	if (start.get_col() == end.get_col()) {
+		if (start.get_row() > end.get_row()) {
+			if ((pos.get_row <= start.get_row()) && (pos.get_row() >= end.get_row())) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else if (start.get_row() < end.get_row()) {
+			if ((pos.get_row >= start.get_row()) && (pos.get_row() <= end.get_row())) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	else if (start.get_row() == end.get_row()) {
+		if (start.get_col() > end.get_col()) {
+			if ((pos.get_col <= start.get_col()) && (pos.get_col() >= end.get_col())) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else if (start.get_col() < end.get_col()) {
+			if ((pos.get_col >= start.get_col()) && (pos.get_col() <= end.get_col())) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
 }
 
 void Ship::hit() {
-    // TODO: write implementation here.
-    return;
+	if (num_hits > size) {
+		num_hits += 1;
+	}
 }
 
 bool Ship::has_sunk() {
-    // TODO: write implementation here.
-    return false;
+	if (num_hits == size) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
