b9d7116392b4142b9e2f380cbc1371d85235ce2c
heschmid <47265951+heschmid@users.noreply.github.com>
2019-03-17 13:28:49 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -121,7 +121,50 @@ void test_ship() {
 }
 
 void test_player() {
-    // Write your tests here
+   	Player p1();
+	Player p2("player1");
+	Player p3("player2");
+	Player p4("player3");
+	Player p5("player4");
+	Player p6("player5");
+
+	Position pos1(5, 4);
+	Position pos2('4', 'A');
+	Position pos3(7, 8);
+	Position pos4('5', 'h');
+	Position pos5(10, 5);
+	Position pos6('7', 'K');
+	Position pos7(-5, -7);
+	Ship ship2(pos1, pos2);
+	Ship ship3(pos2, pos3);
+	Ship ship4(pos3, pos4);
+	Ship ship5(pos4, pos5);
+	Ship ship6(pos5, pos6);
+	Ship ship7(pos6, pos7);
+
+
+	p2.add_ship(ship2);
+	p3.add_ship(ship3);
+	p4.add_ship(ship4);
+	p5.add_ship(ship5);
+	p6.add_ship(ship6);
+
+	p2.position_not_hit(pos1);
+	p3.position_not_hit(pos2);
+	p4.position_not_hit(pos3);
+	p5.position_not_hit(pos4);
+	p6.position_not_hit(pos5);
+
+	p2.attack(p3, pos1);
+	p3.attack(p4, pos2);
+	p4.attack(p5, pos3);
+	p5.attack(p6, pos4);
+	p6.attack(p2, pos5);
+
+	p2.load_grid_file("grid1.txt");
+	p3.load_grid_file("blank.txt");
+	p4.load_grid_file("grid2.txt");
+	p5.load_grid_file("gridB.txt");
     return;
 }
 
