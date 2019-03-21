20c20a37c5ddb1195267b93a331bac88ad93c2fd
heschmid <47265951+heschmid@users.noreply.github.com>
2019-03-21 19:49:48 -0400

    Update Ship.cpp

--- Ship.cpp
+++ Ship.cpp
@@ -4,8 +4,8 @@
  * Ship.cpp
  * Project UID 4cd2d66df3154b6c8b0ff27aa6791edb
  *
- * <Kevin Wang and Heidi Schmidt>
- * <kevwan and heschmid>
+ * <#Heidi Schmidt, Kevin Wang#>
+ * <#heschmid, kevwan#>
  *
  * Project 4: Battleship
  *
@@ -13,10 +13,10 @@
  */
 
 #include <cmath>
-
 #include "Ship.h"
 
 
+//Default constructor
 Ship::Ship() {
 	start.set_col(0);
 	start.set_row(0);
@@ -26,6 +26,8 @@ Ship::Ship() {
 	size = 0;
 }
 
+
+//Non-default constructor
 Ship::Ship(Position start_in, Position end_in) {
 	start = start_in;
 	end = end_in;
@@ -36,21 +38,31 @@ Ship::Ship(Position start_in, Position end_in) {
 	else if (start.get_col() == end.get_col()) {
 		size = abs(start.get_row() - end.get_row()) + 1;
 	}
+	else size = 0;
 }
 
+
+//start getter
 Position Ship::get_start() {
-    return start;
+	return start;
 }
 
+
+//end getter 
 Position Ship::get_end() {
 	return end;
 }
 
+
+//size getter
 int Ship::get_size() {
 	return size;
 }
 
+
+//checks to see if the ship is horizontal 
 bool Ship::is_horizontal() {
+	//If the row values are equal then the ship is horizontal
 	if (start.get_row() == end.get_row()) {
 		return true;
 	}
@@ -59,25 +71,35 @@ bool Ship::is_horizontal() {
 	}
 }
 
+
+//checks if position of the ship is possible
 bool Ship::has_position(Position pos) {
+	//If the col numbers the ship is vertical 
 	if (start.get_col() == end.get_col()) {
+		//if the start of the row is greater than the end
 		if (start.get_row() > end.get_row()) {
+			//makes sure that the position of the row is between the start and end positions
 			if ((pos.get_row() <= start.get_row()) && (pos.get_row() >= end.get_row())) {
 				return true;
 			}
+			//returns false if this is not true
 			else {
 				return false;
 			}
 		}
+		//The program then checks is end is greater than start
 		else if (start.get_row() < end.get_row()) {
+			//makes sure position is between the two points and returns true if it is
 			if ((pos.get_row() >= start.get_row()) && (pos.get_row() <= end.get_row())) {
 				return true;
 			}
+			//Otherwise it returns false
 			else {
 				return false;
 			}
 		}
 	}
+	//Does the same as the above section but for horizontal ships
 	else if (start.get_row() == end.get_row()) {
 		if (start.get_col() > end.get_col()) {
 			if ((pos.get_col() <= start.get_col()) && (pos.get_col() >= end.get_col())) {
@@ -96,15 +118,23 @@ bool Ship::has_position(Position pos) {
 			}
 		}
 	}
+	//returns false if none of the conditions match
 	return false;
 }
 
+
+//Hits the pos of the ship and only does so 
+//if the hit does not sink the ship
 void Ship::hit() {
 	if (num_hits < size) {
 		num_hits += 1;
 	}
 }
 
+
+//Checks to see if the number of hits 
+//is equal to the size of the ship
+//returns true if they are equal 
 bool Ship::has_sunk() {
 	if (num_hits == size) {
 		return true;
